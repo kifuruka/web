5e6e1efb8c2c422dbb1f4bfdea9a0e13fd6ca4e1
noda1985 <noda19851212@gmail.com>
2019-02-19 18:17:02 +0900

    fixed htmlとカルーセル

--- components/Special.vue
+++ components/Special.vue
@@ -1,146 +1,219 @@
 <template>
-  <div class="feature">
-    <h2>特集test</h2>
-    <div class="container_feature">
-      <div class="item">
-        <a href="http://webcreatorbox.com/about">
-          <section class="card">
-            <img class="card-img" src="../assets/images/sankinkoryu.jpg" alt>
-            <div class="card-content">
-              <h3 class="card-title">産近甲龍特集</h3>
+    <div class="feature">
+        <h2>参加学校</h2>
+        <div class="container_feature">
+            <div class="item">
+                <a href="http://webcreatorbox.com/about">
+                    <section class="card">
+                        <div class="card-content">
+                            <h3 class="card-title">麻布学園</h3>
+                        </div>
+                        <img class="card-img" src="../assets/images/azabu_logo.png" alt="">
+                        <div class="card-content">
+                            <h4 class="card-title">活動件数：5件</h4>
+                        </div>
+                    </section>
+                </a>
             </div>
-          </section>
-        </a>
-      </div>
-      <div class="item">
-        <a href="http://webcreatorbox.com/about">
-          <section class="card">
-            <img class="card-img" src="../assets/images/kaiyou.jpg" alt>
-            <div class="card-content">
-              <h3 class="card-title">海洋特集</h3>
+            <div class="item">
+                <a href="http://webcreatorbox.com/about">
+                    <section class="card">
+                        <div class="card-content">
+                            <h3 class="card-title">近畿大学</h3>
+                        </div>
+                        <img class="card-img" src="../assets/images/kindai_logo.png" alt="">
+                        <div class="card-content">
+                            <h4 class="card-title">活動件数：5件</h4>
+                        </div>
+                    </section>
+                </a>
             </div>
-          </section>
-        </a>
-      </div>
-      <div class="item">
-        <a href="http://webcreatorbox.com/about">
-          <section class="card">
-            <img class="card-img" src="../assets/images/ekiden.jpg" alt>
-            <div class="card-content">
-              <h3 class="card-title">駅伝特集</h3>
+            <div class="item">
+                <a href="http://webcreatorbox.com/about">
+                    <section class="card">
+                        <div class="card-content">
+                            <h3 class="card-title">青山学院大学</h3>
+                        </div>
+                        <img class="card-img" src="../assets/images/aoyama_logo.png" alt="">
+                        <div class="card-content">
+                            <h4 class="card-title">活動件数：5件</h4>
+                        </div>
+                    </section>
+                </a>
             </div>
-          </section>
-        </a>
-      </div>
-      <div class="item">
-        <a href="http://webcreatorbox.com/about">
-          <section class="card">
-            <img class="card-img" src="../assets/images/parasports.jpg" alt>
-            <div class="card-content">
-              <h3 class="card-title">パラスポーツ特集</h3>
+            <div class="item">
+                <a href="http://webcreatorbox.com/about">
+                    <section class="card">
+                        <div class="card-content">
+                            <h3 class="card-title">酪農大学</h3>
+                        </div>                        
+                        <img class="card-img" src="../assets/images/rakuno_logo.png" alt="">
+                        <div class="card-content">
+                            <h4 class="card-title">活動件数：5件</h4>
+                        </div>
+                    </section>
+                </a>
             </div>
-          </section>
-        </a>
-      </div>
-      <div class="item">
-        <a href="http://webcreatorbox.com/about">
-          <section class="card">
-            <img class="card-img" src="../assets/images/march.jpg" alt>
-            <div class="card-content">
-              <h3 class="card-title">MARCH特集</h3>
-            </div>
-          </section>
-        </a>
-      </div>
-      <div class="item">
-        <a href="http://webcreatorbox.com/about">
-          <section class="card">
-            <img class="card-img" src="../assets/images/yasai.jpg" alt>
-            <div class="card-content">
-              <h3 class="card-title">農業畜産特集</h3>
-            </div>
-          </section>
-        </a>
-      </div>
-    </div>
-    <!-- <p>{{message}}</p>
-        <p>{{1+3}}</p>
-        <p>{{message ==='test' ? 'OK':'NO' }}</p>
-        <p> {{
-            reversedMessage
-            /* 算出プロパティの値を出⼒ */
-            }} </p>
-            <button v-on:click="reversedMessage">Reverse </button>
-    <input v-model="message" type="text">-->
+        </div>
+
+    <carousel :navigationEnabled="true">
+                    <slide>
+                        <span class="label">
+                            <div class="item">
+                                <a href="http://webcreatorbox.com/about">
+                                    <section class="card">
+                                        <div class="card-content">
+                                            <h3 class="card-title">酪農大学</h3>
+                                        </div>                        
+                                        <img class="card-img" src="../assets/images/rakuno_logo.png" alt="">
+                                        <div class="card-content">
+                                            <h4 class="card-title">活動件数：5件</h4>
+                                        </div>
+                                    </section>
+                                </a>
+                            </div>
+                        </span>
+                      </slide>
+                      <slide>
+                        <span class="label">
+                            <div class="item">
+                                <a href="http://webcreatorbox.com/about">
+                                    <section class="card">
+                                        <div class="card-content">
+                                            <h3 class="card-title">酪農大学</h3>
+                                        </div>                        
+                                        <img class="card-img" src="../assets/images/rakuno_logo.png" alt="">
+                                        <div class="card-content">
+                                            <h4 class="card-title">活動件数：5件</h4>
+                                        </div>
+                                    </section>
+                                </a>
+                            </div>
+                        </span>
+                      </slide>
+                      <slide>
+                        <span class="label">
+                            <div class="item">
+                                <a href="http://webcreatorbox.com/about">
+                                    <section class="card">
+                                        <div class="card-content">
+                                            <h3 class="card-title">酪農大学</h3>
+                                        </div>                        
+                                        <img class="card-img" src="../assets/images/rakuno_logo.png" alt="">
+                                        <div class="card-content">
+                                            <h4 class="card-title">活動件数：5件</h4>
+                                        </div>
+                                    </section>
+                                </a>
+                            </div>
+                        </span>
+                      </slide>
+                      <slide>
+                        <span class="label">
+                            <div class="item">
+                                <a href="http://webcreatorbox.com/about">
+                                    <section class="card">
+                                        <div class="card-content">
+                                            <h3 class="card-title">酪農大学</h3>
+                                        </div>                        
+                                        <img class="card-img" src="../assets/images/rakuno_logo.png" alt="">
+                                        <div class="card-content">
+                                            <h4 class="card-title">活動件数：5件</h4>
+                                        </div>
+                                    </section>
+                                </a>
+                            </div>
+                        </span>
+                    </slide>
+    </carousel>
   </div>
 </template>
 <script>
+import { Carousel, Slide } from 'vue-carousel';
+
 export default {
-  data() {
-    return {
-      message: "teeeest"
-    };
-  },
-  computed: {
-    reversedMessage() {
-      return this.message
-        .split("")
-        .reverse()
-        .join("");
-    }
+    components: {
+    Carousel,
+    Slide
   }
 };
 </script>
 <style>
 .feature {
-  margin: 5% 5%;
+    margin: 5% 0%;
+    padding: 5%;
+    background-color: #333333;
 }
 .container_feature {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 }
-.container h2 {
-  text-align: left;
-  margin-bottom: 1%;
-  padding: 0.25em 0.5em; /*上下 左右の余白*/
-  color: #494949; /*文字色*/
-  background: transparent; /*背景透明に*/
-  border-left: solid 0.4em #ffe65d; /*左線*/
+
+.container h2  {
+  position: relative;
+  display: inline-block;
+  margin-bottom: 2em;
+  color:white;
 }
-.item {
-  margin-bottom: 1%;
+
+.container h2:before {
+  content: '';
+  position: absolute;
+  bottom: -15px;
+  display: inline-block;
+  width: 2.5rem;
+  height: 0.2rem;
+  left: 50%;
+  -webkit-transform: translateX(-50%);
+  transform: translateX(-50%);
+  background-color: #FFE65D;
+  border-radius: 2px;
 }
+
+.container h3  {
+    font-size: 1rem;
+}
+.container h4  {
+    font-size: 0.8rem;
+}
+
+
+.item  {
+    margin-bottom:1%;
+}
+
+
 .item a {
-  text-decoration: none;
+    text-decoration:none;
 }
+
 .card {
-  width: 350px;
-  margin-right: 10px;
-  background: #fff;
-  border-radius: 5px;
-  box-shadow: 0 2px 5px #ccc;
+  width: 200px;
+  margin-right:10px;     
 }
 .card-img {
-  border-radius: 5px 5px 0 0;
+  border-radius: 0 0 0 0;
   max-width: 100%;
   height: auto;
 }
 .card-content {
-  padding: 10px;
+  padding: 1px;
 }
+
 h2 a {
   text-decoration: none;
 }
 .card-title {
-  text-decoration: none;
-  color: #ffffff;
-  font-size: 20px;
-  text-align: center;
-  color: #777;
+    text-decoration: none;
+    color: #ffffff;
+    font-size: 1rem;
+    text-align: left;
+    color: #777;
 }
 .card-title a {
   text-decoration: none;
+
 }
 .card-text {
   color: #777;
@@ -160,4 +233,16 @@ h2 a {
 .card-link a:hover {
   color: #0090aa;
 }
+
+/* スライダーjs */
+
+.carousel {
+  width:80%;
+  border : 1px solid #ccc;
+}
+
+.slide {
+  background-color : pink
+}
+
 </style>
